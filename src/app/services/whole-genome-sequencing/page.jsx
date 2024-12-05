df6fd1a0eeb32e4266160df9bdd93e1f5294ea7b
VladoLov <vladislav.lovric@gmail.com>
2024-12-06 00:11:05 +0100

    deleted old headers1

--- src/app/services/whole-genome-sequencing/page.jsx
+++ src/app/services/whole-genome-sequencing/page.jsx
@@ -1,14 +1,9 @@
-import Header from "@/components/Header";
 import ArticleWholeGenomeSequencing from "../compoents/ArticleWholeGenomeSequencing";
-
-import Footer2 from "@/components/Footer2";
 
 function page() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-start">
-      <Header />
       <ArticleWholeGenomeSequencing />
-      <Footer2 />
     </div>
   );
 }
