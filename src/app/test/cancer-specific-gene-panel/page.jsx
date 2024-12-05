0c8a54b29283a335e4de5aaec69680bcd4799816
VladoLov <vladislav.lovric@gmail.com>
2024-12-06 00:03:56 +0100

    deleted old headers

--- src/app/test/cancer-specific-gene-panel/page.jsx
+++ src/app/test/cancer-specific-gene-panel/page.jsx
@@ -1,12 +1,11 @@
 import React from "react";
-import CancerSpecificGenePanel from "../components/CancerSpecificGenePanel";
+
 import ImageWithText from "../components/ImageWithText";
 
 const page = () => {
   return (
     <div>
       <ImageWithText />
-      <CancerSpecificGenePanel />
     </div>
   );
 };
