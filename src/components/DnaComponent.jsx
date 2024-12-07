9e44d2618ad428fbfb42706c31ebd99edbc1237a
VladoLov <vladislav.lovric@gmail.com>
2024-12-07 18:24:28 +0100

    back good main

--- src/components/DnaComponent.jsx
+++ src/components/DnaComponent.jsx
@@ -27,7 +27,7 @@ export const DnaComponent = () => {
         </div>
         <div className="absolute top-12 w-full flex items-left flex-row justify-center  gap-4 font-oswald">
           <div className="basis-1/2 min-h-[70vh] leading-snug flex flex-col  justify-center items-start  lg:max-w-4xl max-w-xs">
-            <MainHeader asChild />
+            <MainHeader />
           </div>
           <div className="hidden basis-1/2 items-center justify-center lg:flex animate-in slide-in-from-right duration-700 ease-in-out">
             <Image
--- src/newcomponents/MainHeader.jsx
+++ src/newcomponents/MainHeader.jsx
@@ -6,7 +6,7 @@ function MainHeader() {
   const { t } = useTranslation("common");
   return (
     <>
-      <h1 className=" slide-in-from-left  md:px-10 text-start md:leading-relaxed lg:text-8xl  text-4xl sm:text-6xl animate-in transition ease-in-out duration-700 font-bold text-lightBlue-950  drop-shadow-lg py-1 mb-7 !font-oswald">
+      <h1 className=" slide-in-from-left  md:px-10 text-start md:leading-relaxed lg:text-8xl text-4xl sm:text-6xl animate-in transition ease-in-out duration-700 font-bold text-lightBlue-950  drop-shadow-lg py-1 mb-7 !font-oswald">
         {t("header.first")}{" "}
         <span className="text-crimson-500 pr-1"> {t("header.second")}</span>
         {t("header.three")}
